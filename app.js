c297cdaa34136b5887addc728f2c3280fd777ebe
GiftChrisJava <chrisjava77@gmail.com>
2024-11-15 10:30:12 +0200

    update added and query paramiter on the get all books function

--- app.js
+++ app.js
@@ -25,11 +25,18 @@ connectToDb((err) => {
 // });
 
 app.get("/books", (req, res) => {
+  // current
+  const page = req.query.p || 0;
+
+  const booksPerPage = 2;
+
   let books = [];
 
   db.collection("books")
     .find()
     .sort({ author: 1 })
+    .skip(page * booksPerPage)
+    .limit(booksPerPage)
     .forEach((book) => books.push(book))
     .then(() => {
       res.status(200).json(books);
@@ -71,4 +78,47 @@ app.post("/books", (req, res) => {
     .catch((err) => {
       res.status(500).json({ msg: "Error posting book" });
     });
+});
+
+// delete
+app.get("/books/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .deleteOne({ _id: new ObjectId(req.params.id) }) // Use 'new' keyword here
+      .then((doc) => {
+        if (doc) {
+          res.status(200).json(doc); // Corrected status code to 200
+        } else {
+          res.status(404).json({ error: "Document not found" });
+        }
+      })
+      .catch((err) => {
+        console.error("Error fetching document:", err); // Log the error
+        res.status(500).json({ error: "Couldn't delete the document" });
+      });
+  } else {
+    res.status(400).json({ msg: "Invalid id" }); // Use 400 for invalid request
+  }
+});
+
+// update
+app.get("/books/:id", (req, res) => {
+  const updates = req.body;
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .updateOne({ _id: new ObjectId(req.params.id) }, { $set: updates }) // Use 'new' keyword here
+      .then((result) => {
+        if (result) {
+          res.status(200).json(doc); // Corrected status code to 200
+        } else {
+          res.status(404).json({ error: "Document not found" });
+        }
+      })
+      .catch((err) => {
+        console.error("Error fetching document:", err); // Log the error
+        res.status(500).json({ error: "Couldn't update the document" });
+      });
+  } else {
+    res.status(400).json({ msg: "Invalid id" }); // Use 400 for invalid request
+  }
 });
