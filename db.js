98830c5d722aca4842a67ca14ae2d6be59f02f4e
GiftChrisJava <chrisjava77@gmail.com>
2024-11-16 10:19:14 +0200

    atlas uri

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const { MongoClient } = require("mongodb");
 
 let dbConnection;
+let uri =
+  "mongodb+srv://yaza:0000-0000@cluster0.mfaza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 module.exports = {
   connectToDb: (callback) => {
-    MongoClient.connect("mongodb://localhost:27017/bookstore")
+    MongoClient.connect(uri)
       .then((client) => {
         dbConnection = client.db();
         return callback();
